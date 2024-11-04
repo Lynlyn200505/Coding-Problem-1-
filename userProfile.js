f5f0c3677e54a2d1cae6189315c418c886d1f7f0
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-11-04 17:36:27 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Ailyn L. Gasatan";
+let age = 19;
+let favoriteNumber = 17;
+let favoriteColor = "violet";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
